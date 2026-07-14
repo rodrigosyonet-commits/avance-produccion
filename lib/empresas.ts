eabd6dccddc925d692f61507b8340b007c87da42
rodrigosyonet-commits <rodrigo.syonet@gmail.com>
2026-07-14 17:16:35 -0600

    Update empresas.ts

--- lib/empresas.ts
+++ lib/empresas.ts
@@ -1,1 +1,33 @@
 
+export const EMPRESAS = [
+  {
+    id: "jugos",
+    nombre: "Jugos del Valle",
+    rfc: "SCM180807MS9",
+    sucursal: "Matriz"
+  },
+  {
+    id: "santaclara",
+    nombre: "Santa Clara",
+    rfc: "SCM180807MS9-1",
+    sucursal: "Matriz"
+  },
+  {
+    id: "alpura",
+    nombre: "Alpura",
+    rfc: "SCM180807MS9-2",
+    sucursal: "Matriz"
+  },
+  {
+    id: "boboli",
+    nombre: "Boboli",
+    rfc: "SCM180807MS9-5",
+    sucursal: "Matriz"
+  },
+  {
+    id: "jumex",
+    nombre: "Jumex",
+    rfc: "SCM180807MS9-6",
+    sucursal: "Matriz"
+  }
+];
