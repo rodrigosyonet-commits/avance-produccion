b590e22add7f3034f2e46b470de6579ba29d6a61
rodrigosyonet-commits <rodrigo.syonet@gmail.com>
2026-07-14 17:43:22 -0600

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,14 @@
 
+export type Empresa = {
+  nombre: string;
+  rfc: string;
+  sucursal: string;
+};
+
+export type ProduccionRow = {
+  folioOrden: string;
+  "Cantidad a Producir": number;
+  "Cantidad Producida": number;
+  "% Avance": number;
+  Estado: string;
+};
