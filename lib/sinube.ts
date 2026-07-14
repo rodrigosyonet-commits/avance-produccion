d2b32bdf99b356079ed94f68a1fa64ac54b2d7e9
rodrigosyonet-commits <rodrigo.syonet@gmail.com>
2026-07-14 17:17:12 -0600

    Add consultarSiNube function for API requests

--- lib/sinube.ts
+++ lib/sinube.ts
@@ -1,1 +1,31 @@
 
+export async function consultarSiNube(
+  usuario: string,
+  empresa: string,
+  sucursal: string,
+  consulta: string
+) {
+
+  const form = new URLSearchParams();
+
+  form.append("tipo", "3");
+  form.append("emp", empresa);
+  form.append("suc", sucursal);
+  form.append("usu", usuario);
+  form.append(
+    "pas",
+    process.env.SINUBE_PASSWORD!
+  );
+  form.append("cns", consulta);
+
+  const response = await fetch(
+    process.env.SINUBE_URL!,
+    {
+      method: "POST",
+      body: form,
+      cache: "no-store"
+    }
+  );
+
+  return await response.text();
+}
