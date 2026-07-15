a8b785f6d042cf8d2c935ae5671cb9ae87b3b673
rodrigosyonet-commits <rodrigo.syonet@gmail.com>
2026-07-14 18:18:11 -0600

    Fix formatting and remove unnecessary lines in empresas.ts

--- lib/empresas.ts
+++ lib/empresas.ts
@@ -1,33 +1,29 @@
-
 export const EMPRESAS = [
   {
-    id: "jugos",
-    nombre: "Jugos del Valle",
+    nombre:
+      "Jugos del Valle",
     rfc: "SCM180807MS9",
-    sucursal: "Matriz"
+    sucursal: "Matriz",
   },
   {
-    id: "santaclara",
-    nombre: "Santa Clara",
+    nombre:
+      "Santa Clara",
     rfc: "SCM180807MS9-1",
-    sucursal: "Matriz"
+    sucursal: "Matriz",
   },
   {
-    id: "alpura",
     nombre: "Alpura",
     rfc: "SCM180807MS9-2",
-    sucursal: "Matriz"
+    sucursal: "Matriz",
   },
   {
-    id: "boboli",
     nombre: "Boboli",
     rfc: "SCM180807MS9-5",
-    sucursal: "Matriz"
+    sucursal: "Matriz",
   },
   {
-    id: "jumex",
     nombre: "Jumex",
     rfc: "SCM180807MS9-6",
-    sucursal: "Matriz"
-  }
+    sucursal: "Matriz",
+  },
 ];
